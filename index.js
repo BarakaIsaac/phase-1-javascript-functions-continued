acbca99decee9831b7c511307bf0f3c90dcc83a9
BarakaIsaac <isaac.mwandau2@gmail.com>
2022-12-05 16:46:27 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(satActivity = 'roller-skate') {
+    return `This Saturday, I want to ${satActivity}!`;
+}
+saturdayFun();
+
+function mondayWork(monActivity = "go to the office") {
+    return `This Monday, I will ${monActivity}.`;
+}
+mondayWork();
+
+function wrapAdjective(wrap = "*") {
+    function inner(adjective = 'Special') {
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+    return inner;
+}
+
+wrapAdjective();
